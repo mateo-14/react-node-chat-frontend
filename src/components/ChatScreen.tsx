6487fd45e520a34c5217cb4beaa4ca99c0ebac08
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-09 16:32:39 -0300

    style chats

--- src/components/ChatScreen.tsx
+++ src/components/ChatScreen.tsx
@@ -8,11 +8,18 @@ function ChatScreen() {
           <h2 className="sidebar__title">Online users</h2>
         </div>
         <div className="sidebar__chats">
-          <div className="chat-card">
-            <p>
-              asfkasfk ksaf ksafk aksf kasfk askf kasfkasfkaksfkasfk askf kasfkasfk aksf kasfk askf
-            </p>
-          </div>
+          {new Array(10).fill(15).map(() => (
+            <div className="chat-card">
+              <div className="chat-card__avatar"></div>
+              <div className="chat-card__info">
+                <span className="chat-card__username">Mateo</span>
+                <p className="chat-card__lastmsg">
+                  asfkasfk ksaf ksafk aksf kasfk askf kasfkasfkaksfkasfk askf kasfkasfk aksf kasfk
+                  askf
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <div className="chat__main"></div>
