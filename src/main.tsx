cdde2b3d694180cbcdf5f9fe4e42ea2fba4ee400
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-14 14:40:33 -0300

    remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ChatService from './services/chatService';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { authWithUsername } from './slices/chatSlice';
 
 ReactDOM.render(
   <React.StrictMode>
